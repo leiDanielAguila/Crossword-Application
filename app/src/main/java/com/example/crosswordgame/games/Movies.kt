c34f7d89e12860940ade4210bce9166faf7b1cbb
lei <aguila.christleidaniel@gmail.com>
2024-07-16 16:50:32 +0800

    changed tiles

--- app/src/main/java/com/example/crosswordgame/games/Movies.kt
+++ app/src/main/java/com/example/crosswordgame/games/Movies.kt
@@ -220,7 +220,7 @@ fun GameLayout(
                             ) {
                                 Text(text = "")
                             }
-                        }
+                        }   
                     }
                 }
             }
